6c5bed4977e3ab2dca83dc234a537c9dd087dc34
Tangui Morlier <tangui@tangui.eu.org>
2017-03-03 15:17:25 +0100

    Ajout export JSON

--- CFT2HTTP/list.php
+++ CFT2HTTP/list.php
@@ -7,8 +7,14 @@ if (!preg_match('/^reception[a-z0-9\/_]*$/i', $dir)) {
 $prefix = '';
 $postfix = '';
 $isxml = false;
-if (isset($_GET['format']) && $_GET['format'] == 'xml') {
+$isjson = false;
+if (isset($_GET['format'])) {
+  if ($_GET['format'] == 'xml') {
     $isxml = true;
+  }
+  if ($_GET['format'] == 'json') {
+    $isjson = true;
+  }
 }
 $file = tempnam("/tmp/", "list.date");
 if (isset($_GET['from']) && preg_match('/^[0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9][0-9]$/', $_GET['from'])) {
@@ -16,19 +22,33 @@ if (isset($_GET['from']) && preg_match('/^[0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9
 }else{
     exec("touch --date=2016-01-01 $file");
 }
-exec("find $dir -cnewer $file -type f -name '*xml'", $output);
+if (isset($_GET['accise']) && preg_match('/^[A-Z0-9]*$/i',$_GET['accise'])) {
+  exec("find $dir -cnewer $file -type f -name '".$_GET['accise']."*xml'", $output);
+}else{
+  exec("find $dir -cnewer $file -type f -name '*xml'", $output);
+}
 unlink ($file);
 if ($isxml) {
     header("content-type: text/xml\n");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<list>\n";
     $prefix="<url>";
     $postfix="</url>";
-}else{
+}else if ($isjson) {
+  header("content-type: text/json\n");
+  echo '[';
+  $prefix = '"';
+  $postfix = '"';
+}else {
     header("content-type: text/plain\n");
 }
 foreach ($output as $line) {
     print $prefix."http://10.222.223.1/$line".$postfix."\n";
+    if ($prefix == '"') {
+      $prefix = ',"';
+    }
 }
 if ($isxml) {
     echo "</list>\n";
+}else if ($isjson) {
+  echo ']';
 }
