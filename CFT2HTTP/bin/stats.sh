8581b13375369cca54e72c4c97802f7f1eeda0f1
Tangui Morlier <tangui@tangui.eu.org>
2018-01-17 16:58:18 +0100

    Suppression des doublons

--- CFT2HTTP/bin/stats.sh
+++ CFT2HTTP/bin/stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -s https://raw.githubusercontent.com/24eme/mutualisation-douane/master/README.md | awk -F '|' '{print $3}' | sed 's/[,\+]/\n/g' | grep '[0-9]' | sed 's/ *[()] */;/g' | sed 's/^ *//' | awk -F ';' '{print $2";"$1}' | sort  -t ';' -k 1,1 > /tmp/siren2interpro.csv
-find publication_web/reception_douanes/ | sed 's|publication_web/reception_douanes/||'  | sed 's|/|;|' | sed 's|/|-|'  | sed 's|/.*||' | sort | uniq -c  | grep '\-' | sed 's/^ *//' | sed 's/ /;/' | awk -F ';' '{print $3";"$2";"$1}' | sort -t ';' -k 2,2 > /tmp/sirenmois.csv
+find publication_web/reception_douanes/ -type f -name '*xml' | sed 's|publication_web/reception_douanes/||' | sed 's|/|;|' | sed 's|/|-|'  | sed 's|[/_\.]|;|g'  | awk -F ';' '{print $1";"$2";"$3}'  | sort -u | awk -F ';' '{print $1";"$2}' | sort | uniq -c  | grep '\-' | sed 's/^ *//' | sed 's/ /;/' | awk -F ';' '{print $3";"$2";"$1}' | sort -t ';' -k 2,2 > /tmp/sirenmois.csv
 join -t ';' -1 1 -2 2 /tmp/siren2interpro.csv /tmp/sirenmois.csv | awk -F ';' '{print $3";"$2";"$1";"$4}' | sort > publication_web/stats/DRMInterpro.csv.tmp
 mv -f publication_web/stats/DRMInterpro.csv.tmp publication_web/stats/DRMInterpro.csv
 
