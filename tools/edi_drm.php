ec4524c15eb97422b479ffacfeb9ddbafdeaab91
mathurin petit <mathurin.petit@gmail.com>
2018-12-20 16:51:03 +0100

    Page de test des fichiers EDI avec le retour

--- tools/edi_drm.php
+++ tools/edi_drm.php
@@ -1,8 +1,10 @@
-<?php if(isset($_FILES['csv'])):
+<?php
+  if(isset($_FILES['csv']) && isset($_POST['plateforme']) && isset($_POST['userpwd'])):
         $data = file_get_contents($_FILES['csv']['tmp_name']);
-
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://url_edi/");
+        curl_setopt($ch, CURLOPT_URL,$_POST['plateforme']);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ;
+        curl_setopt($ch, CURLOPT_USERPWD, $_POST['userpwd']);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);  //Post Fields
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -16,12 +18,88 @@
 
         $response = curl_exec($ch);
 
-        print_r($response);
+        $isValid = false;
+        $csv_retour = str_getcsv($response,"\n");
+        foreach ($csv_retour as $num_row => $row) {
+          $data_r = explode(";",$row);
+          if(count($data_r) && preg_match("/^OK$/", $data_r[0])){
+            $isValid = true;
+          }
+        }
+        curl_close ($ch);
+?>
+<!doctype html>
+<html lang="fr_FR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	curl_close ($ch);
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-	exit;
-endif; ?>
+    <title>Test du web service DRM - Résultat</title>
+    </head>
+    <body class="bg-light">
+        <div class="container">
+            <div class="py-9 text-center">
+            <img class="d-block mx-auto mb-4" src="//www.24eme.fr/img/24eme.svg" alt="" width="172" height="172">
+            <?php if($isValid): ?>
+              <h3 class="text-success text-center">Le fichier est correct et peut être importé</h3>
+            <?php else: ?>
+              <h3 class="text-danger text-center">Le fichier comporte des problèmes et ne peut pas être importé :</h3>
+            <?php endif; ?>
+            </div>
+            <br/>
+            <div class="row justify-content-sm-center">
+              <?php if(count($csv_retour) > 2 && $isValid): ?>
+              <div class="col-md-12">
+                <div class="row">
+                  <p>Il y a cependant des points d'attention : </p>
+                </div>
+              </div>
+              <?php endif; ?>
+                <div class="col-md-12">
+                  <div class="row">
+                    <table class="table table-striped">
+                      <?php
+                      foreach ($csv_retour as $num_row => $row):
+                        $data_r = explode(";",$row);
+                        if(!$num_row): ?>
+                        <thead>
+                          <tr>
+                            <?php foreach ($data_r as $key => $val): ?>
+                               <th scope="col"><?php echo $val; ?></th>
+                            <?php endforeach; ?>
+                          </tr>
+                        </thead>
+                        <tbody>
+                      <?php elseif(!preg_match("/^OK$/", $data_r[0])):
+                        ?>
+                        <tr>
+                          <?php foreach ($data_r as $key => $val): ?>
+                             <td class="<?php if(!$key && ($val == "Error")): ?>text-danger<?php endif;?><?php if(!$key && ($val == "Warning")): ?>text-warning<?php endif;?>" ><?php echo $val; ?></td>
+                          <?php endforeach; ?>
+                        </tr>
+                      <?php endif;
+                     endforeach;
+                     ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="row">
+                  <a class="btn btn-secondary btn-block" href="/">Retour</a>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+exit;
+endif;
+$config = json_decode(file_get_contents('config.json'),true);
+?>
 <!doctype html>
 <html lang="fr_FR">
   <head>
@@ -46,7 +124,10 @@ endif; ?>
                 <div class="form-group row">
                     <label for="domaine" class="col-sm-4 col-form-label">Plateforme</label>
                     <div class="col-sm-8">
-                        <select class="form-control">
+                        <select class="form-control"  name="plateforme">
+                          <?php foreach ($config["plateformes"] as $applik => $appliv) : ?>
+                            <option value="<?php echo $appliv; ?>"><?php echo $applik; ?></option>
+                          <?php endforeach; ?>
                         </select>
                     </div>
                 </div>
@@ -54,6 +135,7 @@ endif; ?>
                     <label for="csv" class="col-sm-4 col-form-label">Fichier CSV</label>
                     <div class="col-sm-8">
                         <input type="file" name="csv" class="form-control-file" id="csv">
+                        <input type="hidden" name="userpwd" id="userpwd" value="<?php echo $config["userpwd"]; ?>"/>
                     </div>
                 </div>
                 <div class="row py-3">
