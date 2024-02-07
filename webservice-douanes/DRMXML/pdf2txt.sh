2fa1152e0b439b9330c42efb4cc90c662bac0e32
Tangui Morlier <tangui@tangui.eu.org>
2024-02-07 16:36:44 +0100

    PDF avec un espace

--- webservice-douanes/DRMXML/pdf2txt.sh
+++ webservice-douanes/DRMXML/pdf2txt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pdftotext $1
+pdftotext "$1"
 sed -i 's/^Page [0-9]*\/[0-9]*//' CIEL*txt
 sed -i 's/^CIEL[- ]*Contrat de service avec les interprofessions.*odt//' CIEL*txt
 sed -i 's/Ce document est la propriété de la DGDDI et ne peut être ni divulgué ni copié sans son autorisation. Les citations doivent mentionner la source.//' CIEL*txt
